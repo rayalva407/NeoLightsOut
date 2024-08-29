a179e80e88bc04a4dae593d8b2223d894ad33d50
Raymundo Alva <ralv156@wgu.edu>
2024-08-29 13:57:26 -0600

    Creates the views for the fragmanets and activities

--- app/src/main/java/com/rayalva/neolightsout/HelpFragment.java
+++ app/src/main/java/com/rayalva/neolightsout/HelpFragment.java
@@ -14,7 +14,7 @@ public class HelpFragment extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_help_fragment);
+        setContentView(R.layout.help_fragment);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
